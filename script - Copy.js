4e2b4caf3b60fb658ff0209f0e7e03e9f5e07fe1
Priya Srivastava <srivastavapriya14@gmail.com>
2016-07-06 15:35:20 +0530

    Add files via upload

---
+++ script - Copy.js
@@ -0,0 +1,29 @@
+
+(function (window) {
+
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+for (var i = 0; i < names.length; i++) {
+
+ 
+var firstLetter = names[i].charAt(0);
+var firstLetter1 = names[i].charAt(0).toLowerCase();
+
+ 
+if (firstLetter1 === 'j' || firstLetter === 'J') {
+ 
+   byeSpeaker.speak(names[i]);
+  
+}
+else {
+   
+   helloSpeaker.speak(names[i]);
+  
+}
+
+}
+window.names=names;
+}
+)(window);
